c176d17fb75b3548489eddc284c6b818e4e144ab
Anton Kutsim <kutsimanton@gmail.com>
2022-10-10 08:54:55 +0300

    add email

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -10,7 +10,7 @@ export const UserMenu = () => {
 
     return (
         <Box >
-            <p >Welcome, {user.name}</p>
+            <p>{user.email}</p>
             <button type="button" onClick={() => dispatch(logOut())}>
                 Logout
             </button>
